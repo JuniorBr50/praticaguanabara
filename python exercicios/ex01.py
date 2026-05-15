1e9a9e6cde6188276ccdb136dd4cb1709107521b
JuniorBr50 <103380922+JuniorBr50@users.noreply.github.com>
2026-05-15 17:09:21 -0300

    drv

--- python exercicios/ex01.py
+++ python exercicios/ex01.py
@@ -1,8 +1,11 @@
-import random
-numeros=[random.randint(0,10),random.randint(0,10),random.randint(0,10),random.randint(0,10)]
-print("os numeros sortedos foram:",end=" ")
-for c in numeros:
-    print(c,end=" ")
-print(" ")
-print(f" maior:{max(numeros)}")
-print(f" menor :{min(numeros)}")
+l=[]
+while True:
+    numero=int(input("digite o numero: "))
+    if numero not in l:
+        l.append(numero)
+    op=str(input("deseja continuar [S/N] :")).upper()
+    while op not in ["S",'N']:
+        op = str(input("deseja continuar [S/N] :")).upper()
+    if op == "N":
+        break
+print(sorted(l))
